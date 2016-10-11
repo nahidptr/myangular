a7d18f933bf47b8bd6416d0c31b39f07b4d3221b
nahid <nahid.akhtar@suppor.com>
2016-10-11 10:59:07 +0530

    Chapter 10 Done Modules and the Injector

---
+++ src/injector.js
@@ -0,0 +1,86 @@
+'use strict';
+
+
+function createInjector(modulesToLoad, strictDi) {
+
+  var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+  var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+  var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg;
+
+  var cache = {};
+  var loadedModules = {};
+  strictDi = (strictDi === true);
+
+  var $provide = {
+    constant: function (key, value) {
+      if (key === 'hasOwnProperty') {
+        throw 'hasOwnProperty is not a valid constant name!';
+      }
+      cache[key] = value;
+    }
+  };
+
+  function invoke(fn, self, locals) {
+    var args =  _.map(annotate(fn), token => {
+      if (_.isString(token)) {
+        return locals && locals.hasOwnProperty(token) ? locals[token] : cache[token];
+      } else {
+        throw 'Incorrect injection token! Expected a string, got ';
+      }
+    });
+    if(_.isArray(fn)) {
+      fn = _.last(fn);
+    }
+    return fn.apply(self, args);
+  }
+
+  function annotate(fn) {
+    if (_.isArray(fn)) {
+      return fn.slice(0, fn.length - 1);
+    } else if(fn.$inject){
+      return fn.$inject;
+    } else if(!fn.length) {
+      return [];
+    } else {
+      if(strictDi) {
+        throw 'fn is not using explicit annotation and cannot be invoked in strict mode';
+      }
+      var source = fn.toString().replace(STRIP_COMMENTS, '');
+      var argDeclaration = source.match(FN_ARGS);
+      return _.map(argDeclaration[1].split(','), (argName) => argName.match(FN_ARG)[2]);
+    }
+  }
+
+  function instantiate(Type, locals) {
+    var UnWrappedType = _.isArray(Type) ? _.last(Type) : Type;
+    var instance = Object.create(UnWrappedType.prototype);
+    invoke(Type, instance, locals);
+    return instance;
+  }
+
+  _.forEach(modulesToLoad, function loadModule(moduleName) {
+
+    if (!loadedModules.hasOwnProperty(moduleName)) {
+      loadedModules[moduleName] = true;
+      var module = angular.module(moduleName);
+      _.forEach(module.requires, loadModule);
+      _.forEach(module._invokeQueue, invokeArgs => {
+        var method = invokeArgs[0];
+        var args = invokeArgs[1];
+        $provide[method].apply($provide, args);
+      });
+    }
+  });
+
+  return {
+    has: function (key) {
+      return cache.hasOwnProperty(key);
+    },
+    get: function (key) {
+      return cache[key];
+    },
+    invoke: invoke,
+    annotate: annotate,
+    instantiate: instantiate
+  }
+}
